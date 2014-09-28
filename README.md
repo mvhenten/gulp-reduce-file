687cd52a7e8b7898436744a2771b7410f25d0636
Matthijs van Henten <matthijs@ischen.nl>
2014-09-29 00:09:02 +0200

    upated examples and adds more tests

--- README.md
+++ README.md
@@ -3,8 +3,12 @@ gulp-reduce-file
 
 Reduce files to one single output using custom callbacks.
 
+This module provides (me) a little extra plumbing where `gulp-concat` or `gulp-compile-hogan` don't
+provide the flexibility I need, while while abstracting the `streams` related boilerplate away.
 
 ## Usage
+
+Trivial example of providing a directory listing as json object:
 
 ```javascript
 
@@ -28,3 +32,67 @@ Reduce files to one single output using custom callbacks.
     // produces a dist/filelist.json containing a list of files in src
 
 ```
+
+### Examples
+
+### 1. use case rendering templates
+
+Suppose you have a view structure where partials per template are contained in a sub directory,
+and you want to maintain that structure pre-rendered:
+
+    views/index/template.html
+    views/index/partials/head.html
+    views/index/partials/foooter.html
+
+```javascript
+    var gulp = require('gulp');
+    var reduce = require('gulp-reduce-files');
+
+    function collect( file, templates ){
+        var stack = templates,
+            parts = file.relative.split('/'),
+            compiled = hogan.compile( file.contents.toString('utf8'), { asString: true } );
+
+        while( parts.length ){
+            var part = parts.shift();
+            if( parts.length == 0 ) stack[part] = compiled;
+            stack = stack[part] = stack[part] || {};
+        }
+
+        return templates;
+    }
+
+    function end( templates ){
+        return 'module.exports = ' + JSON.stringify(templates, null, 2) + ';';
+    }
+
+    gulp.task('templates', function () {
+        return gulp.src('views/**/*.html')
+            .pipe(reduce('translations.json', collect, end, {} ) )
+            .pipe(gulp.dest('client'));
+    });
+
+```
+
+### 2. concatinating translations
+
+Suppose you'd want to bundle a bunch of `.json` translations into a single module:
+
+    var gulp = require('gulp');
+    var reduce = require('gulp-reduce-files');
+
+    function collect( file, memo ){
+        var locale = file.relative.split('/')[0];
+        memo[locale] = JSON.parse( file.contents );
+        return memo;
+    }
+
+    function end( memo ){
+        return 'module.exports = ' + JSON.stringify(memo, null, 2);
+    }
+
+    gulp.task('translations', function () {
+        return gulp.src('i18n/*.json')
+            .pipe(reduce('translations.json', collect, end, {} ) )
+            .pipe(gulp.dest('client'));
+    });
--- test/test.js
+++ test/test.js
@@ -3,8 +3,10 @@
 var streamArray = require('stream-array'),
     File = require('gulp-util').File,
     test = require('tape'),
+    async = require('async'),
+    Faker = require('faker'),
     streamAssert = require('stream-assert'),
-    gulpReduceFile = require('../index');
+    reduce = require('../index');
 
 
 function testfile() {
@@ -12,6 +14,7 @@ function testfile() {
 
     function create(contents) {
         return new File({
+            path: contents,
             contents: new Buffer(contents),
         });
     }
@@ -20,7 +23,7 @@ function testfile() {
 }
 
 test('Memo is passed and wrapped into a file', function(assert) {
-    var reduce = gulpReduceFile('out.js', function(file, memo) {
+    var iterator = reduce('out.js', function(file, memo) {
         return memo;
     }, function(memo) {
         assert.deepEqual(memo, {}, 'Got the memo unchanged');
@@ -28,9 +31,89 @@ test('Memo is passed and wrapped into a file', function(assert) {
     }, {});
 
     testfile('test')
-        .pipe(reduce)
-        .pipe(streamAssert.first(function(reduced) {
-            assert.equal(reduced.contents.toString(), '{}', 'Memo was stringified');
+        .pipe(iterator)
+        .pipe(streamAssert.first(function(outfile) {
+            assert.equal(outfile.contents.toString(), '{}', 'Memo was stringified');
             assert.end();
         }));
+});
+
+test('Just multiple files', function(assert) {
+    var names = Faker.Lorem.words(),
+        files = testfile.apply(null, names);
+
+    var iterator = reduce('out.js', function(file, memo) {
+        memo[file.relative] = true;
+        return memo;
+
+    }, function(memo) {
+        return memo;
+    }, {});
+
+    files
+        .pipe(iterator)
+        .pipe(streamAssert.first(function(outfile) {
+            var expect = names.reduce(function(expect, name) {
+                expect[name] = true;
+                return expect;
+            }, {});
+
+            assert.equal(outfile.contents.toString(), JSON.stringify(expect, null, 2));
+            assert.end();
+        }));
+});
+
+test('Memocan be a string, array, object', function(assert) {
+    var name = Faker.Lorem.words().join('/');
+
+    var cases = [
+        {
+            label: 'Memo can be a string',
+            memo: '',
+            file: name,
+            outfile: 'Got: ' + name,
+            iterator: function(file, memo) {
+                return memo + 'Got: ' + file.relative;
+            }
+        },
+        {
+            label: 'Memo can be an array',
+            memo: [],
+            file: name,
+            outfile: JSON.stringify([name], null, 2),
+            iterator: function(file, memo) {
+                memo.push(file.relative);
+                return memo;
+            }
+        },
+        {
+            label: 'Memo can be an object',
+            memo: {},
+            file: name,
+            get outfile() {
+                var out = {};
+                out[name] = true;
+                return JSON.stringify(out, null, 2);
+            },
+            iterator: function(file, memo) {
+                memo[file.relative] = true;
+                return memo;
+            }
+        }
+    ];
+
+    async.each(cases, function(testCase, next) {
+        var iterator = reduce('out.js', testCase.iterator, function(memo) {
+            return memo;
+        }, testCase.memo);
+
+        testfile(testCase.file)
+            .pipe(iterator)
+            .pipe(streamAssert.first(function(outfile) {
+                assert.equal(outfile.contents.toString(), testCase.outfile, testCase.label);
+                next();
+            }));
+
+    }, assert.end);
+
 });
