a64a3d5ec22a291a1d32e23824ab3bb1ebff1081
Ivan Khilko <ikhilko.dev@gmail.com>
2015-10-27 14:56:17 +0300

    call "iterate" and "end" with stream context

--- index.js
+++ index.js
@@ -11,13 +11,15 @@ module.exports = function reduce(filename, fn, done, memo) {
     if (!done instanceof Function) throw new PluginError('gulp-reduce-file', 'Missing done callback for gulp-reduce-file');
 
     memo = memo || '';
+    
+    fn = fn.apply(this, [file, memo]);
 
     function iterate(file) {
         memo = fn(file, memo);
     }
 
     function end() {
-        var content = done(memo),
+        var content = done.apply(this, [memo]),
             target = new File();
 
         if (typeof content === 'string') content = new Buffer(content);
